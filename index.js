fcaa958c0a733e5d036517221dfdbf3e41e84368
Yash Jaiswal <jais.yash01@gmail.com>
2020-05-27 18:49:36 +0530

    now tweet stream working with twitter package/ also tried twit package

--- index.js
+++ index.js
@@ -2,19 +2,36 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = 3010
-// const Twitter = require('twitter')  // not working
-/*
-// not working
+const Twitter = require('twitter')
+// var Twit = require('twit')
+
+// var T = new Twit({
+//     consumer_key: '881flbXb16SbwdP3R2sRKvdji',
+//     consumer_secret: '1lgXzKUdWoMzdvba4r1YEY7gAEXqlMOSRMq99Iz7mUM2nNikFi',
+//     access_token: '1132229170388332545-Cw7n7UQzfMXUn8fkKvU8g3JPUXG7iK',
+//     access_token_secret: 'TZxdpxpwV5ys7F9bxZkK7hoW3ErpBJUfwBX6MZMINgCpg'
+//     // timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+//     // strictSSL: true,     // optional - requires SSL certificates to be valid.
+// })
+
+// var stream = T.stream('statuses/filter', { track: 'mango' })
+
+// stream.on('tweet', function (tweet) {
+//     console.log(tweet)
+// })
+
+
+// now working
 
 const twit = new Twitter({
     consumer_key: '881flbXb16SbwdP3R2sRKvdji',
     consumer_secret: '1lgXzKUdWoMzdvba4r1YEY7gAEXqlMOSRMq99Iz7mUM2nNikFi',
-    access_token_key: '1132229170388332545-j5aLvteIDWWuSyHyJPvqXgsHi3DMpP',
-    access_token_secret: 'ISSVhx8UxSnDZ7cX1TklJ5BfYHIbZrGi2hJa8Sb97G2i7'
+    access_token_key: '1132229170388332545-Cw7n7UQzfMXUn8fkKvU8g3JPUXG7iK',
+    access_token_secret: 'TZxdpxpwV5ys7F9bxZkK7hoW3ErpBJUfwBX6MZMINgCpg'
 })
 
 // You can also get the stream in a callback if you prefer.
-twit.stream('statuses/filter', { track: 'javascript' }, function (stream) {
+twit.stream('statuses/filter', { track: 'modi' }, function (stream) {
 
     stream.on('data', function (event) {
         console.log(event)
@@ -24,7 +41,7 @@ twit.stream('statuses/filter', { track: 'javascript' }, function (stream) {
         console.log(error)
     })
 });
-*/
+
 
 
 app.listen(port, () => {
